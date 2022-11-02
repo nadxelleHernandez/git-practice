a8386d70ce6897a4110e98e1df39eace36e46e43
Xuan Hien Pham <phamxuanhien02@gmail.com>
2022-11-01 21:34:05 -0400

    function d commit

--- function_d.py
+++ function_d.py
@@ -2,7 +2,11 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    largest_num = 0
+    for num in numbers:
+        if largest_num < num:
+            largest_num = num
+    return largest_num        
 
 
 if __name__ == "__main__":
